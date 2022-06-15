b342cad6697c7d9c7bb0cd00a46d192d9e7057b6
Grégoire DURAND <59779735+gregoire-drd@users.noreply.github.com>
2022-06-15 23:42:14 +0200

    Update and rename OE-MC-Install.sh to MC-Install.sh

--- MC-Install.sh
+++ MC-Install.sh
@@ -73,7 +73,7 @@ case $version in
     read -p "Type : " type
     echo
     case $type in
-    Vanilla | vanilla | V | v ) type="vanilla" ; server_jar="https://launcher.mojang.com/mc/game/1.7.2/server/3716cac82982e7c2eb09f83028b555e9ea606002/server.jar";;
+    Vanilla | vanilla | V | v ) type="vanilla" ; server_jar="https://launcher.mojang.com/mc/game/1.7.10/server/952438ac4e01b4d115c5fc38f891710c4941df29/server.jar";;
     Bukkit | bukkit | B | b ) type="bukkit" ; server_jar="https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.7.10-R0.1-20140808.005431-8.jar";;
     Spigot | spigot | S | s ) type="spigot" ; server_jar="https://cdn.getbukkit.org/spigot/spigot-1.7.10-SNAPSHOT-b1657.jar";;
     *) echo "ERREUR - Invalid input" ; echo ; exit 0 ;;
@@ -87,9 +87,9 @@ case $version in
     read -p "Type : " type
     echo
     case $type in
-    Vanilla | vanilla | V | v ) type="vanilla" ; server_jar="https://launcher.mojang.com/mc/game/1.7.2/server/3716cac82982e7c2eb09f83028b555e9ea606002/server.jar";;
+    Vanilla | vanilla | V | v ) type="vanilla" ; server_jar="https://launcher.mojang.com/mc/game/1.8/server/a028f00e678ee5c6aef0e29656dca091b5df11c7/server.jar";;
     Bukkit | bukkit | B | b ) type="bukkit" ; server_jar="https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.7.10-R0.1-20140808.005431-8.jar";;
-    Spigot | spigot | S | s ) type="spigot" ; server_jar="https://cdn.getbukkit.org/spigot/spigot-1.7.10-SNAPSHOT-b1657.jar";;
+    Spigot | spigot | S | s ) type="spigot" ; server_jar="https://cdn.getbukkit.org/spigot/spigot-1.8-R0.1-SNAPSHOT-latest.jar";;
     *) echo "ERREUR - Invalid input" ; echo ; exit 0 ;;
     esac
 ;;
@@ -229,7 +229,7 @@ case $version in
     case $type in
     Vanilla | vanilla | V | v ) type="vanilla" ; server_jar="https://launcher.mojang.com/v1/objects/3cf24a8694aca6267883b17d934efacc5e44440d/server.jar";;
     Bukkit | bukkit | B | b ) type="bukkit" ; server_jar="https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.7.10-R0.1-20140808.005431-8.jar";;
-    Spigot | spigot | S | s ) type="spigot" ; server_jar="https://cdn.getbukkit.org/spigot/spigot-1.7.10-SNAPSHOT-b1657.jar";;
+    Spigot | spigot | S | s ) type="spigot" ; server_jar="https://download.getbukkit.org/spigot/spigot-1.18.jar";;
     *) echo "ERREUR - Invalid input" ; echo ; exit 0 ;;
     esac
 ;;
@@ -245,6 +245,21 @@ case $version in
     Bukkit | bukkit | B | b ) type="bukkit" ; server_jar="https://download.getbukkit.org/craftbukkit/craftbukkit-1.18.2.jar";;
     Spigot | spigot | S | s ) type="spigot" ; server_jar="https://download.getbukkit.org/spigot/spigot-1.18.2.jar";;
     Paper | paper | P | p ) type="paper" ; server_jar="https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/290/downloads/paper-1.18.2-290.jar";;
+    *) echo "ERREUR - Invalid input" ; echo ; exit 0 ;;
+    esac
+;;
+
+1.19 | 119)
+    version="1.19"
+    echo "                  [Vanilla] [Bukkit] [Spigot] [Paper]                  "
+    echo
+    read -p "Type : " type
+    echo
+    case $type in
+    Vanilla | vanilla | V | v ) type="vanilla" ; server_jar="https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar";;
+    Bukkit | bukkit | B | b ) type="bukkit" ; server_jar="https://download.getbukkit.org/craftbukkit/craftbukkit-1.19.jar";;
+    Spigot | spigot | S | s ) type="spigot" ; server_jar="https://download.getbukkit.org/spigot/spigot-1.19.jar";;
+    Paper | paper | P | p ) type="paper" ; server_jar="https://api.papermc.io/v2/projects/paper/versions/1.19/builds/18/downloads/paper-1.19-18.jar";;
     *) echo "ERREUR - Invalid input" ; echo ; exit 0 ;;
     esac
 ;;
